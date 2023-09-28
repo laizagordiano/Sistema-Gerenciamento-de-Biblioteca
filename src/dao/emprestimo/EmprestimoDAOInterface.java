1bf158d9ed6664e33fdae4f42e6f1ab62071bfac
laizagordiano <laizagordiano.5@gmail.com>
2023-09-28 16:40:30 -0300

    :sparkles: feat: Criação de classes DAO e interfaces

--- src/dao/emprestimo/EmprestimoDAOInterface.java
+++ src/dao/emprestimo/EmprestimoDAOInterface.java
@@ -2,7 +2,12 @@ package dao.emprestimo;
 
 import dao.CRUD;
 import model.Emprestimo;
+import model.Leitor;
+import model.Livro;
+
+import java.util.List;
 
 public interface EmprestimoDAOInterface extends CRUD<Emprestimo, Exception> {
-
+    public Emprestimo findEmprestimo(Leitor leitor, Livro livro);
+    public List<Emprestimo> findByLeitor(Leitor leitor);
 }
