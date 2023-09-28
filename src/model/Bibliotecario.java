b03808e06d15591d5a855a47ae0226c9480c50a0
laizagordiano <laizagordiano.5@gmail.com>
2023-09-28 16:19:21 -0300

    :recycle: refactor: Alterações na Classe Bibliotecario

--- src/model/Bibliotecario.java
+++ src/model/Bibliotecario.java
@@ -1,8 +1,16 @@
 package model;
 
 public class Bibliotecario extends Usuario{
-
+    private String cargo;
     public Bibliotecario(String nome, String senha, String cargo) {
-        super(nome, senha, cargo);
+        super(nome, senha);
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
     }
 }
