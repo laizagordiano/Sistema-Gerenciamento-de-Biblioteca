9751acdbcea8eb13f257badfd507066e0c622b39
laizagordiano <laizagordiano.5@gmail.com>
2023-09-28 16:18:33 -0300

    :recycle: refactor: Alterações na Classe Administrador

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -1,9 +1,17 @@
 package model;
 
 public class Administrador extends Usuario{
+    private String cargo;
     public Administrador(String nome, String senha, String cargo){
-        super(nome, senha, cargo);
+        super(nome, senha);
 
     }
 
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 }
