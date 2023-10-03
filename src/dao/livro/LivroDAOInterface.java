02fc5299b8db8769126e9fbf41c344b3fae9a241
laizagordiano <laizagordiano.5@gmail.com>
2023-10-02 21:47:32 -0300

    :books: docs: Documentando as interfaces

--- src/dao/livro/LivroDAOInterface.java
+++ src/dao/livro/LivroDAOInterface.java
@@ -4,11 +4,46 @@ import dao.CRUD;
 import model.Livro;
 
 import java.util.ArrayList;
-
+/** Essa interface contém métodos que correspondem as informações do livro, como:
+ * Encontrar por ISBN;
+ * Encontrar por autor;
+ * Encontar por Categoria;
+ * Encontrar por Título.
+ * @author Laiza Araujo Gordiano Oliveira
+ * @see dao.CRUD
+ * @see model.Livro
+ */
 public interface LivroDAOInterface extends CRUD<Livro, Exception> {
+    /**
+     * Esse método tem como objetivo encontrar e retornar uma lista de livros que têm um número ISBN específico.
+     * @param isbn
+     * @return
+     * @throws Exception
+     */
     public ArrayList<Livro> findISBN(String isbn) throws Exception;
+
+    /**
+     * Esse método tem como objetivo encontrar e retornar uma lista de livros que têm um autor específico.
+     * @param autor
+     * @return
+     * @throws Exception
+     */
     public ArrayList<Livro> findAutor(String autor) throws Exception;
+
+    /**
+     * Esse método tem como objetivo encontrar e retornar uma lista de livros que têm uma categoria específica.
+     * @param categoria
+     * @return
+     * @throws Exception
+     */
     public ArrayList<Livro> findCategoria(String categoria) throws Exception;
+
+    /**
+     * Esse método tem como objetivo encontrar e retornar uma lista de livros que têm um título específico
+     * @param titulo
+     * @return
+     * @throws Exception
+     */
     public ArrayList<Livro> findTitulo(String titulo) throws Exception;
 
 }
--- src/dao/reserva/ReservaDAOInterface.java
+++ src/dao/reserva/ReservaDAOInterface.java
@@ -5,10 +5,53 @@ import model.Leitor;
 import model.Livro;
 import model.Reserva;
 
+/**
+ * Essa interface contém métodos que correspondem as informações da reserva, como:
+ * Procurar por reservas específicas;
+ * Procurar por leitor na reserva;
+ * Verifica se o leitor é o primeiro na lista de reservas;
+ * Verifica se há reservas;
+ * Retorna o número de livros renovados.
+ * @author Laiza Araujo Gordiano Oliveira
+ * @see model.Leitor
+ * @see model.Livro,
+ * @see model.Reserva
+ */
 public interface ReservaDAOInterface extends CRUD<Reserva, Exception> {
+    /**
+     * Esse método procura e retorna uma reserva específica associada a um leitor e a um livro dentro
+     * de uma lista de reservas. Se encontrar, retorna a reserva; caso contrário, retorna null
+     * @param leitor
+     * @param livro
+     * @return
+     */
     public Reserva findReservas(Leitor leitor, Livro livro);
+
+    /**
+     * Esse método procura e retorna uma reserva específica associada a um leitor dentro de uma lista de reservas.
+     * Se encontrar, retorna a reserva; caso contrário, retorna null
+     * @param leitor
+     * @return
+     */
     public Reserva findLeitorNaReserva(Leitor leitor);
+
+    /**
+     * Esse método verifica se o leitor fornecido é o primeiro na lista de reservas associadas a ele.
+     *  Se for o caso, retorna true; caso contrário, retorna false.
+     * @param leitor
+     * @return
+     */
     public boolean primeiroLeitor(Leitor leitor);
+
+    /**
+     * Esse método verifica se existem reservas na lista.
+     * @return
+     */
     public boolean haReservas();
+
+    /**
+     * Esse método conta o número de livros distintos reservados.
+     * @return
+     */
     public int numReservados();
 }
