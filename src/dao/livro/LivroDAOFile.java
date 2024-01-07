96d869e6aa0d58feecb3afaa27035b722bfee105
laizagordiano <laizagordiano.5@gmail.com>
2024-01-07 17:40:08 -0300

    :zap: perf: Implemetação do DAOFile

---
+++ src/dao/livro/LivroDAOFile.java
@@ -0,0 +1,147 @@
+package dao.livro;
+
+import armazenamento.FileMethods;
+import exceptions.LivroException;
+import model.Livro;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * A classe implementa a interface LivroDAOInterface e fornece métodos
+ * para realizar operações relacionadas a livros, como criação, exclusão, atualização
+ * e consulta.
+ * @author Laiza Araujo Gordiano Oliveira
+ */
+public class LivroDAOFile implements LivroDAOInterface {
+    File arquivo;
+    private static final String NAMEFILE= "livros";
+
+    public LivroDAOFile(){
+        arquivo = FileMethods.createFile(NAMEFILE);
+    }
+
+
+    private int getProximoID(ArrayList<Livro> livros){
+        int cont = -1;
+        for (Livro l : livros){
+            cont++;
+        }
+        return (cont+1);
+    }
+
+
+    @Override
+    public Livro create(Livro obj) {
+        ArrayList<Livro> livros = findMany() ;
+        obj.setId(this.getProximoID(livros));
+        livros.add(obj);
+        FileMethods.sobreescreverArquivo(arquivo, livros);
+        return obj;
+    }
+
+    @Override
+    public void delete(Livro obj) throws LivroException {
+        ArrayList<Livro> listLivros = findMany();
+        boolean deletou = listLivros.remove(obj);
+        if (!deletou){
+            throw new LivroException(LivroException.DELETAR,obj);
+        }
+        FileMethods.sobreescreverArquivo(arquivo, listLivros);
+    }
+
+    @Override
+    public void deleteMany() {
+        FileMethods.apagarConteudoArquivo(arquivo);
+
+    }
+
+    @Override
+    public Livro update(Livro obj) throws LivroException {
+        ArrayList<Livro> listLivros = findMany();
+        int index = listLivros.indexOf(obj);
+        if (index == -1){
+            throw new LivroException(LivroException.ATUALIZAR,obj);
+        }
+        listLivros.set(index, obj);
+        FileMethods.sobreescreverArquivo(arquivo,listLivros);
+        return obj;
+    }
+
+    @Override
+    public ArrayList<Livro> findMany() {
+        return  FileMethods.consultarArquivoList(arquivo);
+    }
+
+    @Override
+    public Livro findById(int id) throws LivroException {
+        ArrayList<Livro> listLivros = findMany();
+        for (Livro livros : listLivros) {
+            if (Objects.equals(livros.getId(), id)) {
+                return livros;
+            }
+        }
+        throw new LivroException(LivroException.PROCURAR);
+    }
+
+    @Override
+    public ArrayList<Livro> findISBN(String isbn) throws LivroException {
+        ArrayList<Livro> listLivros = findMany();
+        ArrayList<Livro> listISBN = new ArrayList<>();
+        for (Livro livros : listLivros) {
+            if (livros.getISBN().equals(isbn)) {
+                listISBN.add(livros);
+            }
+        }
+        if (listISBN.isEmpty()) {
+            throw new LivroException(LivroException.SEM_ISBN);
+        }
+        return listISBN;
+    }
+
+    @Override
+    public ArrayList<Livro> findAutor(String autor) throws LivroException {
+        ArrayList<Livro> listLivros = findMany();
+        ArrayList<Livro> listAutor = new ArrayList<>();
+        for (Livro livros : listLivros) {
+            if (livros.getAutor().equals(autor)) {
+                listAutor.add(livros);
+            }
+        }
+        if (listAutor.isEmpty()) {
+            throw new LivroException(LivroException.SEM_AUTOR);
+        }
+        return listAutor;
+    }
+
+    @Override
+    public ArrayList<Livro> findCategoria(String categoria) throws LivroException {
+        ArrayList<Livro> listLivros = findMany();
+        ArrayList<Livro> listCategoria = new ArrayList<>();
+        for (Livro livros : listLivros) {
+            if (livros.getCategoria().equals(categoria)) {
+                listCategoria.add(livros);
+            }
+        }
+        if (listCategoria.isEmpty()) {
+            throw new LivroException(LivroException.SEM_CATEGORIA);
+        }
+        return listCategoria;
+    }
+
+    @Override
+    public ArrayList<Livro> findTitulo(String titulo) throws LivroException {
+        ArrayList<Livro> listLivros = findMany();
+        ArrayList<Livro> listTitulos = new ArrayList<>();
+        for (Livro livros : listLivros) {
+            if (livros.getTitulo().equals(titulo)) {
+                listTitulos.add(livros);
+            }
+        }
+        if (listTitulos.isEmpty()) {
+            throw new LivroException(LivroException.SEM_EXEMPLAR);
+        }
+        return listTitulos;
+    }
+}
---
+++ src/dao/reserva/ReservaDAOFile.java
@@ -0,0 +1,142 @@
+package dao.reserva;
+
+import armazenamento.FileMethods;
+import exceptions.EmprestimoException;
+import exceptions.LivroException;
+import exceptions.ReservaException;
+import model.Emprestimo;
+import model.Leitor;
+import model.Livro;
+import model.Reserva;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A classe implementa a interface ReservaDAOInterface e fornece métodos
+ * para realizar operações relacionadas a reservas, como criação, exclusão, atualização
+ * e consulta.
+ * @author Laiza Araujo Gordiano Oliveira
+ */
+public class ReservaDAOFile implements ReservaDAOInterface{
+    File arquivo;
+    private static final String NAMEFILE= "reservas";
+
+    public ReservaDAOFile(){
+        arquivo = FileMethods.createFile(NAMEFILE);
+    }
+
+
+    private int getProximoID(ArrayList<Reserva> reservas){
+        int cont = -1;
+        for (Reserva l : reservas){
+            cont++;
+        }
+        return (cont+1);
+    }
+    @Override
+    public Reserva create(Reserva obj) {
+        ArrayList<Reserva> reservas = findMany() ;
+        obj.setId(this.getProximoID(reservas));
+        reservas.add(obj);
+        FileMethods.sobreescreverArquivo(arquivo, reservas);
+        return obj;
+
+    }
+
+    @Override
+    public void delete(Reserva obj) throws ReservaException {
+        ArrayList<Reserva> listReservas = findMany();
+        boolean deletou = listReservas.remove(obj);
+        if (!deletou){
+            throw new ReservaException(ReservaException.DELETAR,obj);
+        }
+        FileMethods.sobreescreverArquivo(arquivo, listReservas);
+
+    }
+
+    @Override
+    public void deleteMany() {
+        FileMethods.apagarConteudoArquivo(arquivo);
+
+    }
+
+    @Override
+    public Reserva update(Reserva obj) throws ReservaException {
+        ArrayList<Reserva> listReservas = findMany();
+        int index = listReservas.indexOf(obj);
+        if (index == -1){
+            throw new ReservaException(ReservaException.ATUALIZAR,obj);
+        }
+        listReservas.set(index, obj);
+        FileMethods.sobreescreverArquivo(arquivo,listReservas);
+        return obj;
+    }
+
+    @Override
+    public ArrayList<Reserva> findMany() {
+        return  FileMethods.consultarArquivoList(arquivo);
+    }
+
+    @Override
+    public Reserva findById(int id) throws ReservaException {
+        ArrayList<Reserva> listReservas = findMany();
+        for (Reserva reserva: listReservas){
+            if (Objects.equals(reserva.getId(), id)){
+                return reserva;
+            }
+        }
+        throw new ReservaException(ReservaException.PROCURAR);
+    }
+
+    @Override
+    public Reserva findReservas(Leitor leitor, Livro livro) throws ReservaException {
+        ArrayList<Reserva> listReservas = findMany();
+        for (Reserva reserva : listReservas) {
+            if (reserva.getLeitor().equals(leitor) && reserva.getLivro().equals(livro)) {
+                return reserva;
+            }
+        }
+        throw new ReservaException(ReservaException.PROCURAR);
+    }
+
+    @Override
+    public Reserva findLeitorNaReserva(Leitor leitor) throws ReservaException{
+        ArrayList<Reserva> listReservas = findMany();
+        for (Reserva reserva : listReservas) {
+            if (reserva.getLeitor().equals(leitor)) {
+                return reserva;
+            }
+        }
+        throw new ReservaException(ReservaException.PROCURAR);
+    }
+
+    @Override
+    public boolean primeiroLeitor(Leitor leitor) throws ReservaException {
+        ArrayList<Reserva> listReservas = findMany();
+        Reserva primeiraReserva = findLeitorNaReserva(leitor);
+        return primeiraReserva != null && listReservas.indexOf(primeiraReserva) == 0;
+    }
+
+    @Override
+    public boolean haReservas() {
+        ArrayList<Reserva> listReservas = findMany();
+        return !listReservas.isEmpty();
+    }
+
+    @Override
+    public int numReservados() {
+        ArrayList<Reserva> listReservas = findMany();
+        int cont = 0;
+        List<String> isbnContabilizados = new ArrayList<>();
+        for (Reserva reserva : listReservas) {
+            if (!isbnContabilizados.contains(reserva.getLivro().getISBN())) {
+                isbnContabilizados.add(reserva.getLivro().getISBN());
+                cont++;
+            }
+        }
+        return cont;
+    }
+}
